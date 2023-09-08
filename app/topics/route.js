9ee0789b2adda52ee9de622a75690312166596a7
Monde Sineke <nsineke@gmail.com>
2023-09-08 09:29:43 +0200

    ✨ feat(topics): UPdte route
    Make CRUD operations for topics route

--- app/topics/route.js
+++ app/topics/route.js
@@ -6,6 +6,8 @@ topics.add("cars");
 topics.add("bolas");
 topics.add("music");
 
+const otherTopics = ["cars", "bolas", "music"];
+
 // console.log("fooSet size, 1st Time >>>", fooSet.size);
 
 // console.log("Is there a music topic:", fooSet.has("music"));
@@ -18,14 +20,34 @@ export async function GET(request) {
   let searchTopic = request.nextUrl.searchParams.get("topic");
 
   const topicExists = topics.has(searchTopic);
+  // const otherTopicExists = otherTopics.find((topic) => topic === searchTopic);
 
   let message = "";
 
+  console.log("topics >>>", topics);
+
   if (topicExists) {
-    message = "Topic exists";
+    message = `Topic ${searchTopic} exists`;
   } else {
-    message = "No Dice. Topic does not exist";
+    message = `No Dice. ${searchTopic} Topic does not exist`;
   }
 
   return NextResponse.json({ data: message });
+}
+
+export async function POST(request) {
+  let res = await request.json();
+  console.log("request >>>", res.topic);
+  topics.add(res.topic);
+  console.log("topics >>>", topics);
+  return NextResponse.json({ message: "something Happened" });
+}
+
+export async function DELETE(request) {
+  let res = await request.json();
+  console.log("request >>>", res.topic);
+  topics.delete(res.topic);
+  console.log("topics >>>", topics);
+
+  return NextResponse.json({ message: "Deleted the topic" });
 }
