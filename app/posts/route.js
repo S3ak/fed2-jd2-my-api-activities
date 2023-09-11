c2cfe235edd17fde0cc340a7dc4908e7d0fb0c21
Monde Sineke <nsineke@gmail.com>
2023-09-11 13:30:42 +0200

    ✨ feat(posts): Return Fake data
    Use faker to generate fake data which will be return by the posts API.

--- app/posts/route.js
+++ app/posts/route.js
@@ -1,5 +1,19 @@
 import { NextResponse } from "next/server";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.sentence(),
+    body: faker.lorem.paragraph(),
+    author: faker.person.firstName("female"),
+  };
+}
 
 export async function GET(request) {
-  return NextResponse.json({ posts: "Success" }, { status: 200 });
+  const fakePosts = faker.helpers.multiple(createRandomPost, {
+    count: 10,
+  });
+
+  return NextResponse.json({ posts: fakePosts }, { status: 200 });
 }
