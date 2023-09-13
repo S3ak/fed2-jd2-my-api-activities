0fc296e084b11bdf7668a463a95feda51863a367
Monde Sineke <nsineke@gmail.com>
2023-09-13 17:56:52 +0200

    🐳 chore(nextConfig): Handle images
    Handle images from cloudflare

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    loader: "custom",
+    loaderFile: "./lib/cloudflare-provider.js",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
